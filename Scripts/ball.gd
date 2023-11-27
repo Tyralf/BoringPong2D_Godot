635d2c0d4e78506e625607f1003c9d54933546da
Tyralf <dorian.h-g@hotmail.fr>
2023-11-27 07:14:10 +0100

    Ajout du joueur 2, binding des touches, et ajout de la balle. Rangement rapide des fichiers du projet

---
+++ Scripts/ball.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var speed = 200
+
+func _ready():
+	velocity.x = 1*speed
+	velocity.y = 1*speed
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+
